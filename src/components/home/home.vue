f06a7ca89923a05de3c6c7eea286f56aaba3604f
Administrator <Administrator@MOP6EXV9E5J1M1F>
2020-02-28 18:35:52 +0800

    添加左侧导航栏+调整样式和图标

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,27 +1,92 @@
 <template>
-  <el-container class="container">
+<el-container class="container">
     <el-header class="header">
-      <el-row>
-        <el-col :span="4">
-          <div class="grid-content bg-purple">
-            <img src="../../assets/logo.png" alt="无法显示图片" />
-          </div>
-        </el-col>
-        <el-col :span="18" class="middle">
-          <h2>电商系统后台管理平台</h2>
-        </el-col>
-        <el-col :span="2">
-          <div class="grid-content bg-purple">
-            <a href="#" class="loginout">退出</a>
-          </div>
-        </el-col>
-      </el-row>
+        <el-row>
+            <el-col :span="4">
+                <div class="grid-content bg-purple">
+                    <img src="../../assets/logo.png" alt="无法显示图片" />
+                </div>
+            </el-col>
+            <el-col :span="18" class="middle">
+                <h2>电商系统后台管理平台</h2>
+            </el-col>
+            <el-col :span="2">
+                <div class="grid-content bg-purple">
+                    <a href="#" class="loginout">退出</a>
+                </div>
+            </el-col>
+        </el-row>
     </el-header>
     <el-container>
-      <el-aside class="aside" width="200px">Aside</el-aside>
-      <el-main class="main">Main</el-main>
+        <el-aside class="aside" width="200px">
+            <el-menu :unique-opened="true" default-active="2" class="el-menu-vertical-demo">
+                <el-submenu index="1">
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>用户管理</span>
+                    </template>
+                    <el-menu-item index="1-1">
+                        <i class="el-icon-menu"></i>
+                        <span>用户列表</span>
+                    </el-menu-item>
+                </el-submenu>
+                <el-submenu index="2">
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>权限管理</span>
+                    </template>
+                    <el-menu-item index="2-1">
+                        <i class="el-icon-success"></i>
+                        <span>角色列表</span>
+                    </el-menu-item>
+                    <el-menu-item index="2-2">
+                        <i class="el-icon-menu"></i>
+                        <span>权限列表</span>
+                    </el-menu-item>
+                </el-submenu>
+                <el-submenu index="3">
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>商品管理</span>
+                    </template>
+                    <el-menu-item index="3-1">
+                        <i class="el-icon-menu"></i>
+                        <span>商品列表</span>
+                    </el-menu-item>
+                    <el-menu-item index="3-2">
+                        <i class="el-icon-menu"></i>
+                        <span>分类参数</span>
+                    </el-menu-item>
+                    <el-menu-item index="3-3">
+                        <i class="el-icon-menu"></i>
+                        <span>商品分类</span>
+                    </el-menu-item>
+                </el-submenu>
+                <el-submenu index="4">
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>订单管理</span>
+                    </template>
+                    <el-menu-item index="4-1">
+                        <i class="el-icon-menu"></i>
+                        <span>订单列表</span>
+                    </el-menu-item>
+                </el-submenu>
+                <el-submenu index="5">
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>报表管理</span>
+                    </template>
+                    <el-menu-item index="5-1">
+                        <i class="el-icon-menu"></i>
+                        <span>数据统计</span>
+                    </el-menu-item>
+                </el-submenu>
+            </el-menu>
+        </el-aside>
+        <el-main class="main">Main</el-main>
     </el-container>
-  </el-container>
+</el-container>
 </template>
 
 <script>
@@ -30,26 +95,28 @@ export default {}
 
 <style>
 .container {
-  height: 100%;
+    height: 100%;
 }
 
 .header {
-  background-color: #b3c0d1;
+    background-color: #b3c0d1;
 }
 
 .aside {
-  background-color: #d3dce6;
+    background-color: #d3dce6;
 }
 
 .main {
-  background-color: #e9eef3;
-  height: 100%;
+    background-color: #e9eef3;
+    height: 100%;
 }
+
 .middle {
-  text-align: center;
+    text-align: center;
 }
+
 .loginout {
-  line-height: 60px;
-  text-decoration: none;
+    line-height: 60px;
+    text-decoration: none;
 }
 </style>
