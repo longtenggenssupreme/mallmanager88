c89d2369bd83afd0c0d06fa614a1436517fc3269
Administrator <Administrator@MOP6EXV9E5J1M1F>
2020-02-28 17:49:33 +0800

    首页-头部样式调整

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,6 +1,22 @@
 <template>
   <el-container class="container">
-    <el-header class="header">Header</el-header>
+    <el-header class="header">
+      <el-row>
+        <el-col :span="4">
+          <div class="grid-content bg-purple">
+            <img src="../../assets/logo.png" alt="无法显示图片" />
+          </div>
+        </el-col>
+        <el-col :span="18" class="middle">
+          <h2>电商系统后台管理平台</h2>
+        </el-col>
+        <el-col :span="2">
+          <div class="grid-content bg-purple">
+            <a href="#" class="loginout">退出</a>
+          </div>
+        </el-col>
+      </el-row>
+    </el-header>
     <el-container>
       <el-aside class="aside" width="200px">Aside</el-aside>
       <el-main class="main">Main</el-main>
@@ -16,14 +32,24 @@ export default {}
 .container {
   height: 100%;
 }
+
 .header {
   background-color: #b3c0d1;
 }
+
 .aside {
   background-color: #d3dce6;
 }
+
 .main {
   background-color: #e9eef3;
   height: 100%;
+}
+.middle {
+  text-align: center;
+}
+.loginout {
+  line-height: 60px;
+  text-decoration: none;
 }
 </style>
